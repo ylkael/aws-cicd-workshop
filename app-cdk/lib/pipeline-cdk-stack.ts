5faa5662e7b0a358847ed851c2ddc3991a6b2dab
Ylkael <ylkael@hotmail.com>
2023-03-30 13:46:52 +0300

    codebuild

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -16,6 +17,15 @@ export class PipelineCdkStack extends Stack {
       pipelineName: 'CICD_Pipeline',
       crossAccountKeys: false,
     });
+
+    const codeQualityBuild = new codebuild.PipelineProject(this, 'Code Quality', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE
+        }
+    });
+    
 
     new CfnOutput(this, 'CodeCommitRepositoryUrl', { value: sourceRepo.repositoryCloneUrlHttp });
 
