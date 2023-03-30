91cea843e1ff6eb39a216e129e066bd438f31ad4
Ylkael <ylkael@hotmail.com>
2023-03-30 15:16:37 +0300

    define cluster

--- app-cdk/lib/app-cdk-stack.ts
+++ app-cdk/lib/app-cdk-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps, Duration } from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 
 interface ConsumerProps extends StackProps {
   ecrRepository: ecr.Repository;
@@ -13,6 +14,10 @@ export class AppCdkStack extends Stack {
     super(scope, `${id}-app-stack`, props);
 
     const vpc = new ec2.Vpc(this, `${id}Vpc`);
+
+    const cluster = new ecs.Cluster(this, `${id}EcsCluster`, {
+      vpc: vpc
+    });    
 
   }
 }
