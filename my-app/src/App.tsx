7eacdaf602a1d15c2215c42ed38bffa3cf8af26e
Ylkael <ylkael@hotmail.com>
2023-03-30 15:40:29 +0300

    continous delivery test

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Test Change!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
