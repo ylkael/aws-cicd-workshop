3f53fe99ddab4edeb9d0de788f263dbd37cdf29e
Ylkael <ylkael@hotmail.com>
2023-03-30 14:00:23 +0300

    codebuild & stage + action

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -17,7 +18,8 @@ export class PipelineCdkStack extends Stack {
       pipelineName: 'CICD_Pipeline',
       crossAccountKeys: false,
     });
-
+    
+    // Create a codebuild pipeline project for our Code Quality build stage
     const codeQualityBuild = new codebuild.PipelineProject(this, 'Code Quality', {
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
@@ -26,6 +28,36 @@ export class PipelineCdkStack extends Stack {
         }
     });
     
+    // Add Artifacts for our source and unit test stages
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    // Add a stage and action for our source control repository
+    pipeline.addStage({
+      stageName: 'Source',
+        actions: [
+          new codepipeline_actions.CodeCommitSourceAction({
+            actionName: 'CodeCommit',
+            repository: sourceRepo,
+            output: sourceOutput,
+            branch: "main",
+          }),
+        ],
+    });
+    
+    // Add a stage and action for our unit testing / code quality
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+        actions: [
+          new codepipeline_actions.CodeBuildAction({
+            actionName: 'Unit-Test',
+            project: codeQualityBuild,
+            input: sourceOutput,
+            outputs: [unitTestOutput],
+          }),
+        ],
+    });
+
 
     new CfnOutput(this, 'CodeCommitRepositoryUrl', { value: sourceRepo.repositoryCloneUrlHttp });
 
