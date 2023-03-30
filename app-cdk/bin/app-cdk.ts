add79ab4511b043f0765e9cd43fdc4658e70137c
Ylkael <ylkael@hotmail.com>
2023-03-30 14:44:32 +0300

    ref ecr stack

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -2,14 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AppCdkStack } from '../lib/app-cdk-stack';
-
 import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
+
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
 new AppCdkStack(app, 'AppCdkStack', {
   
 });
 
-new PipelineCdkStack(app, 'pipeline-stack', {
-  
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', { 
+  ecrRepository: ecrCdkStack.repository,
 });
