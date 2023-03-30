33f4ec76acbe3bb96ed2e7d5d61360882541cde7
Ylkael <ylkael@hotmail.com>
2023-03-30 15:15:49 +0300

    define vpc

--- app-cdk/lib/app-cdk-stack.ts
+++ app-cdk/lib/app-cdk-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps, Duration } from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 interface ConsumerProps extends StackProps {
   ecrRepository: ecr.Repository;
@@ -10,6 +11,8 @@ export class AppCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: ConsumerProps) {
     // use the id here as a prefix to identify our test and prod stacks on creation
     super(scope, `${id}-app-stack`, props);
+
+    const vpc = new ec2.Vpc(this, `${id}Vpc`);
 
   }
 }
