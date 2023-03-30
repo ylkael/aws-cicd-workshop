abb8ee31db2e051cda18a2a86f80da9dec32e651
Ylkael <ylkael@hotmail.com>
2023-03-30 13:40:26 +0300

    codepipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -9,6 +10,11 @@ export class PipelineCdkStack extends Stack {
     const sourceRepo = new codecommit.Repository(this, 'CICD_Workshop', {
       repositoryName: 'CICD_Workshop',
       description: 'Repository for my application code and infrastructure',
+    });
+
+    const pipeline = new codepipeline.Pipeline(this, 'CICD_Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
     });
 
     new CfnOutput(this, 'CodeCommitRepositoryUrl', { value: sourceRepo.repositoryCloneUrlHttp });
